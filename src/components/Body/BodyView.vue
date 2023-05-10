7808bf305395baaf2847b371a76488dd13dcd2d9
Orlov_MD <machello1995@gmail.com>
2023-05-10 23:56:16 +0300

    n

--- src/components/Body/BodyView.vue
+++ src/components/Body/BodyView.vue
@@ -50,7 +50,21 @@ export default {
         }
     },
     methods: {
+        onSendData() {
+            const data = {
+                product: this.product,
+                user: user,
+                queryId
+            }
 
+            fetch('http://localhost:8000', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            })
+        },
     },
 }
 
