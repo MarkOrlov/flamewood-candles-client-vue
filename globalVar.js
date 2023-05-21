4e70edb3208f1cdf20f5be2d640125e193c2fa73
Orlov_MD <machello1995@gmail.com>
2023-05-21 12:53:53 +0300

    regru

--- globalVar.js
+++ globalVar.js
@@ -1,4 +1,4 @@
 export default {
-    // serverAdress: 'http://213.189.201.88:8000'
-    serverAdress: 'http://localhost:8000'
+    serverAdress: 'http://213.189.201.88:8000'
+    // serverAdress: 'http://localhost:8000'
 }
